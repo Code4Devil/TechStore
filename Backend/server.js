1085e3f950e36390232bbbf2781bac82dbde7e89
Code4Devil <code4devil@gmail.com>
2025-04-13 00:08:08 +0530

    bcrypt error fix maybe

---
+++ Backend/server.js
@@ -0,0 +1,4 @@
+// This file is a redirect to server-test.js
+// It exists to maintain compatibility with systems that might be looking for server.js
+console.log('Redirecting from server.js to server-test.js');
+require('./server-test.js');
